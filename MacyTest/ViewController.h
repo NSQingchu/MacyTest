446b58cac1f8e1c69e130d640dfd07ac09b1fe53
Mingyuan Chen <mingyuan@mingyuans-macbook-pro-2.local>
2016-02-16 14:15:01 -0500

    Initial Commit

---
+++ MacyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacyTest
+//
+//  Created by Mingyuan Chen on 2/16/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MacyTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MacyTest
+//
+//  Created by Mingyuan Chen on 2/16/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
